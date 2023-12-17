8471a937672ab3c14f416b090bd263c75885ae09
yuz2577 <u_1379@naver.com>
2023-12-17 21:37:24 +0900

    express연동작업 및 UI 기초세팅

---
+++ blogserver/app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/'); //모든 신호를 받아들인다
+
+export default app;
---
+++ blogserver/server.js
@@ -0,0 +1,6 @@
+import app from './app';
+
+//port 7000번에서 듣고있으면(7000번켜지면) 실행함수 실행해라
+app.listen('7000', () => {
+    console.log('hi');
+});
