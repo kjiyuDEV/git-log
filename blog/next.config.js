c1c84044883515c19bd284261085fb4944efc70b
kjiyuDEV <kjiyu1379@gmail.com>
2024-01-12 01:27:02 +0900

    ?

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    async generateStaticParams(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
         return {
             '/': { page: '/src/app/page' }, // 페이지 경로 설정
         };
