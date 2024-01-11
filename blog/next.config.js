ded50cedff42163b4425dafb14a5422336855eb0
kjiyuDEV <kjiyu1379@gmail.com>
2024-01-12 00:45:50 +0900

    난 next13

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    trailingSlash: true,
-    exportTrailingSlash: true,
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
 };
 
 module.exports = nextConfig;
