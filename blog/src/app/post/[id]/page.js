f200ae3130bedc600604b4b4b05ff148f2b1ed1f
kjiyuDEV <kjiyu1379@gmail.com>
2024-01-17 01:23:50 +0900

    genetartestaticparams

--- blog/src/app/post/[id]/page.js
+++ blog/src/app/post/[id]/page.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Post from './Post';
-import { useSelector } from 'react-redux';
 import { useParams } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 export async function generateStaticParams() {
-    return;
+    const posts = await fetch(`${process.env.NEXT_PUBLIC_BASIC_SERVER_URL}/api/post/skip/0`).then((res) => res.json());
+    const res = [];
+    posts.postsList.map((v, i) => {
+        res.push({ id: v._id });
+    });
+    return res;
 }
 
 export default function Page({ params }) {
